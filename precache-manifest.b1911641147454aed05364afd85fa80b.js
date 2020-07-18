8c29e9c1a2596d6816e3cd5203c03a7dc0842bfd
Michael Yang <myang1@wayfair.com>
2020-07-18 02:27:04 -0400

    Updates

---
+++ precache-manifest.b1911641147454aed05364afd85fa80b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d65ec3704d2f513ccd93772741cd85",
+    "url": "/invex-simulator/index.html"
+  },
+  {
+    "revision": "20a44588195b0e3be247",
+    "url": "/invex-simulator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e86d6eef6e83df454d43",
+    "url": "/invex-simulator/static/js/2.e872335f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/invex-simulator/static/js/2.e872335f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a44588195b0e3be247",
+    "url": "/invex-simulator/static/js/main.f3dad13f.chunk.js"
+  },
+  {
+    "revision": "f73ecfeefb6af87c541a",
+    "url": "/invex-simulator/static/js/runtime-main.efdcf7a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/invex-simulator/static/media/logo.5d5d9eef.svg"
+  }
+]);
